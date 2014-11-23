ad468f53d361ac575d89d533fe6ecc49a1595fb6
Uku Taht <uku.taht@gmail.com>
2014-11-23 20:04:56 +0000

    add build script

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "tictactoe"
+
+version := "0.1"
+
+scalaVersion := "2.11.2"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.1.3" % "test"
---
+++ src/test/scala/BoardSpec.scala
@@ -0,0 +1,10 @@
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
+
+class BoardSpec extends FunSpec with ShouldMatchers {
+  describe("Board") {
+    it("is true") {
+      1 should equal(1)
+    }
+  }
+}
