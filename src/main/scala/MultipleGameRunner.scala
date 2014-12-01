dcc71d21e1f81b13baa28060b529520c9411f8b2
Uku Taht <uku.taht@gmail.com>
2014-12-01 13:34:01 +0000

    wraps tests around player selections

--- src/main/scala/MultipleGameRunner.scala
+++ src/main/scala/MultipleGameRunner.scala
@@ -1,4 +1,4 @@
-package ttt.game_runner
+package ttt.multiple_game_runner
 
 import ttt.board.Board
 import ttt.game.Game
@@ -11,37 +11,45 @@ import ttt.negamax_player.NegamaxPlayer
 
 import java.io.{PrintWriter, BufferedReader, InputStreamReader}
 
-object GameRunner {
+object MultipleGameRunner {
   val in  = new BufferedReader(new InputStreamReader(System.in))
   val out = new PrintWriter(System.out)
   val display = new ConsoleDisplay(in, out)
 
   def main(args: Array[String]) = {
-    new GameRunner(display).playUntilPlayerQuits()
+    new MultipleGameRunner(display).playUntilPlayerQuits()
   }
 }
 
-class GameRunner(private val display: Display) {
-  private val gameTypes = Map(
-    "Human vs Human"       -> { build(human(X), human(O)) },
-    "Human vs Computer"    -> { build(human(X), computer(O)) },
-    "Computer vs Human"    -> { build(computer(X), human(O)) },
-    "Computer vs Computer" -> { build(computer(X), computer(O)) }
+class MultipleGameRunner(private val display: Display) {
+  private val playerTypes = Map(
+    "Human vs Human"       -> List(human(X), human(O)),
+    "Human vs Computer"    -> List(human(X), computer(O)),
+    "Computer vs Human"    -> List(computer(X), human(O)),
+    "Computer vs Computer" -> List(computer(X), computer(O))
   )
 
   def playUntilPlayerQuits() = {
     var playAgain = true
     while(playAgain) {
-      gameTypes(selectGame()).play()
+      selectGame.play()
       playAgain = display.playAgain()
     }
   }
 
-  private def selectGame() = {
-    display.select("game type", gameTypes.keys.toList).get
+  def selectPlayers() = {
+    var selection: Option[String] = None
+    while(!selection.isDefined) {
+      selection = display.select("players", playerTypes.keys.toList)
+    }
+    playerTypes(selection.get)
   }
 
-  private def build(players: Player*) = {
+  private def selectGame = {
+    build(selectPlayers())
+  }
+
+  private def build(players: Seq[Player]) = {
     new Game(Board.empty, display, players)
   }
 
---
+++ src/test/scala/MultipleGameRunnerSpec.scala
@@ -0,0 +1,84 @@
+package ttt.multiple_game_runner
+
+import ttt.console_display.ConsoleDisplay
+import ttt.console_player.ConsolePlayer
+import ttt.negamax_player.NegamaxPlayer
+import ttt.player.Player
+
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
+import org.scalamock.scalatest.MockFactory
+import java.io.{Writer, BufferedReader, StringWriter, StringReader}
+
+class MultipleGameRunnerSpec extends FunSpec with ShouldMatchers with BeforeAndAfter{
+
+  var out: Writer  = _
+
+  before {
+    out = new StringWriter()
+  }
+
+  def withInStr(str: String, testFn: MultipleGameRunner => Unit) = {
+    val in = new BufferedReader(new StringReader(str))
+    testFn(new MultipleGameRunner(new ConsoleDisplay(in, out)))
+  }
+
+  def selectPlayers(selection: String, testFn: Seq[Player] => Unit) = {
+    val in = new BufferedReader(new StringReader(selection))
+    val runner = new MultipleGameRunner(new ConsoleDisplay(in, out))
+    testFn(runner.selectPlayers)
+  }
+
+  describe("selecting players") {
+    it("human vs human") {
+      selectPlayers("1\n", players => {
+        players.head      shouldBe a [ConsolePlayer]
+        players.tail.head shouldBe a [ConsolePlayer]
+      })
+    }
+
+    it("human vs computer") {
+      selectPlayers("2\n", players => {
+        players.head      shouldBe a [ConsolePlayer]
+        players.tail.head shouldBe a [NegamaxPlayer]
+      })
+    }
+
+    it("computer vs human") {
+      selectPlayers("3\n", players => {
+        players.head      shouldBe a [NegamaxPlayer]
+        players.tail.head shouldBe a [ConsolePlayer]
+      })
+    }
+
+    it("computer vs computer") {
+      selectPlayers("4\n", players => {
+        players.head      shouldBe a [NegamaxPlayer]
+        players.tail.head shouldBe a [NegamaxPlayer]
+      })
+    }
+
+    it("gets input until valid selection is given") {
+      selectPlayers("bad\n99\n\n4\n", players => {
+        players.head      shouldBe a [NegamaxPlayer]
+        players.tail.head shouldBe a [NegamaxPlayer]
+      })
+    }
+  }
+
+  describe("game runner") {
+    it("plays through one game") {
+      withInStr("1\n1\n2\n3\n4\n5\n6\n7\nno\n", runner => {
+        runner.playUntilPlayerQuits()
+        out.toString should include("X has won")
+      })
+    }
+
+    it("plays through two games") {
+      withInStr("1\n1\n2\n3\n4\n5\n6\n7\ny\n1\n1\n2\n3\n4\n5\n6\n7\nno\n", runner => {
+        runner.playUntilPlayerQuits()
+        out.toString should include("X has won")
+      })
+    }
+  }
+}
